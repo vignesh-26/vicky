e5a6227f4043bab3b5022c07eb21813d7a926f02
vignesh-26 <52446286+vignesh-26@users.noreply.github.com>
2019-07-08 22:47:11 +0530

    Create square_or_not.py

---
+++ square_or_not.py
@@ -0,0 +1,12 @@
+a1,a2=list(map(int,input().split()))
+b1,b2=list(map(int,input().split()))
+c1,c2=list(map(int,input().split()))
+d1,d2=list(map(int,input().split()))
+m=b2-a2
+n=c2-d2
+o=c1-b1
+p=d1-a1
+if(m==n==o==p):
+  print("yes")
+else:
+  print("no")
