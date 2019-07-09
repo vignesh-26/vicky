efbfadee41f7de6ffe3f2caddb025f82509c4008
vignesh-26 <52446286+vignesh-26@users.noreply.github.com>
2019-07-09 19:57:10 +0530

    Update even-odd_odd-even_hunter.py

--- even-odd_odd-even_hunter.py
+++ even-odd_odd-even_hunter.py
@@ -2,6 +2,6 @@ n=int(input())
 k=list(map(int,input().split()))
 for i in range(0,n):
   if(i%2==0 and k[i]%2==1):
-    print(k[i],end="")
+    print(k[i],end=" ")
   if(i%2==1 and k[i]%2==0):
-    print(k[i],end="")
+    print(k[i],end=" ")
