665a2d3bb248ff45be23f8e3c5eb0b895b3f1736
vignesh-26 <52446286+vignesh-26@users.noreply.github.com>
2019-07-09 23:48:07 +0530

    Update maxvalue_PQR_pro.py

--- maxvalue_PQR_pro.py
+++ maxvalue_PQR_pro.py
@@ -1,8 +1,8 @@
 a,b,c,d=map(int,input().split())
 t=list(map(int,input().split()))
-v=[]
+z=[]
 for i in range(0,len(t)):
   for j in range(i,len(t)):
     for k in range(j,len(t)):
-      v.append(b*t[i]+c*t[j]+d*t[k])
-print(max(v))
+      z.append(b*t[i]+c*t[j]+d*t[k])
+print(max(z))
